3604b10290dbcda0c228ff0101da4a19daa4d6d4
Marco Antônio <ma.souza.junior@gmail.com>
2025-09-01 23:33:01 -0300

    fea: add create budget form

--- lib/budgie_web/live/budget_list_live.ex
+++ lib/budgie_web/live/budget_list_live.ex
@@ -16,10 +16,35 @@ defmodule BudgieWeb.BudgetListLive do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
+      <.modal
+        :if={@live_action == :new}
+        id="create-budget-modal"
+        on_cancel={JS.navigate(~p"/budgets", replace: true)}
+        on_confirm={JS.navigate(~p"/budgets")}
+      >
+        <span class="text-lg font-bold">Create Budget</span>
+
+      <.live_component
+        module={BudgieWeb.CreateBudgetDialog}
+        id="create-budget"
+        current_user={@current_scope.user}
+      />
+
+      </.modal>
+
       <div class="mx-auto max-w-3xl py-6">
-        <h1 class="text-2xl font-bold mb-4">Budget List</h1>
-        <!-- Budget list content goes here -->
-        <p>This is where the budget list will be displayed.</p>
+        <h1 class="text-2xl font-bold mb-4 flex justify-between">
+          <spam>Budgets</spam>
+
+          <.link
+            class="btn btn-primary mb-4 right"
+            phx-click={JS.navigate(~p"/budgets/new")}
+            id="new-budget-button"
+          >
+            <.icon name="hero-plus" class="h-4 w-4" />
+            <span class="ml-2">New Budget</span>
+          </.link>
+        </h1>
 
         <.table id="budgets" rows={@budgets}>
           <:col :let={budget} label="Name">{budget.name}</:col>
@@ -29,6 +54,52 @@ defmodule BudgieWeb.BudgetListLive do
         </.table>
       </div>
     </Layouts.app>
+    """
+  end
+
+  @doc """
+  Modal dialog.
+  """
+  attr :id, :string, required: true
+  slot :inner_block, required: true
+  attr :on_cancel, JS, default: %JS{}
+  attr :on_confirm, JS, default: %JS{}
+
+  def modal(assigns) do
+    ~H"""
+    <dialog
+      id={@id}
+      phx-hook="Modal"
+      phx-remove={
+        JS.remove_attribute("open")
+        |> JS.transition({"ease-out duration-200", "opacity-100", "opacity-0"}, time: 0)
+      }
+      data-cancel={JS.exec(@on_cancel, "phx-remove")}
+      class="modal"
+      open
+    >
+      <.focus_wrap
+        id={"#{@id}-container"}
+        phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
+        phx-key="escape"
+        phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
+        class="modal-box"
+      >
+        <.button
+          phx-click={JS.exec("data-cancel", to: "##{@id}")}
+          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 tx-lg"
+        >
+          ✕
+        </.button>
+
+        {render_slot(@inner_block)}
+
+        <div class="flex justify-end gap-2 mt-8">
+          <.button phx-click={@on_cancel}>Cancel</.button>
+          <.button phx-click={@on_confirm} variant="primary">Confirm</.button>
+        </div>
+      </.focus_wrap>
+    </dialog>
     """
   end
 end
---
+++ lib/budgie_web/live/create_budget_dialog.ex
@@ -0,0 +1,21 @@
+defmodule BudgieWeb.CreateBudgetDialog do
+  use BudgieWeb, :live_component
+
+  alias Budgie.Tracking
+  alias Budgie.Tracking.Budget
+
+  @impl true
+  def update(assigns, socket) do
+    changeset =
+      Tracking.change_budget(%Budget{
+        creator_id: assigns.current_user.id
+      })
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(form: to_form(changeset))
+
+    {:ok, socket}
+  end
+end
