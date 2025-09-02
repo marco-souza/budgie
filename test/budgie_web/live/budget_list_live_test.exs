9100a4f5c85442df5bdad85a6d515dfa110126e1
Marco Antônio <ma.souza.junior@gmail.com>
2025-09-02 13:13:30 -0300

    fea: set test cases for create budget form

--- test/budgie_web/live/budget_list_live_test.exs
+++ test/budgie_web/live/budget_list_live_test.exs
@@ -1,6 +1,7 @@
 defmodule BudgieWeb.BudgetListLiveTest do
   use BudgieWeb.ConnCase, async: true
 
+  import Phoenix.HTML
   import Phoenix.LiveViewTest
   import Budgie.TrackingFixtures
 
@@ -17,10 +18,41 @@ defmodule BudgieWeb.BudgetListLiveTest do
       conn = log_in_user(conn, user)
       {:ok, lv, html} = live(conn, ~p"/budgets")
 
-      open_browser(lv)
+      # open_browser(lv)
 
       assert html =~ budget.name
       assert html =~ budget.description
+    end
+  end
+
+  describe "Create Budget modal" do
+    test "moda is presented", %{conn: conn, user: user} do
+      element_id = "#create-budget-modal"
+      conn = log_in_user(conn, user)
+
+      {:ok, lv, _html} = live(conn, ~p"/budgets/new")
+
+      assert has_element?(lv, element_id)
+    end
+
+    test "modal is presented with form errors", %{conn: conn, user: user} do
+      element_id = "#create-budget-modal"
+      conn = log_in_user(conn, user)
+
+      {:ok, lv, _html} = live(conn, ~p"/budgets/new")
+
+      form = form(lv, "#{element_id} form")
+
+      html =
+        render_change(form, %{
+          "budget" => %{
+            "name" => "Hello"
+          }
+        })
+
+      {:safe, output} = html_escape("can't be blank")
+
+      assert html =~ "#{output}"
     end
   end
 end
